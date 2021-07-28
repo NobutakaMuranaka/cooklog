b64acddb3ee2f3d8e2c019cf03efcefeac045529
EC2 Default User <ec2-user@ip-172-31-15-235.us-east-2.compute.internal>
2021-07-28 13:14:38 +0000

    料理モデルの作成、料理一覧の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :dishes, dependent: :destroy
   attr_accessor :remember_token
   before_save :downcase_email
   validates :name, presence: true, length: { maximum: 50 }
@@ -21,23 +22,30 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
-  end
-
-  # 永続セッションのためにユーザーをデータベースに記憶する
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-  end
-
-  # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
   
-  def forget
-    update_attribute(:remember_digest, nil)
   end
+
+    # 永続セッションのためにユーザーをデータベースに記憶する
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # 渡されたトークンがダイジェストと一致したらtrueを返す
+    def authenticated?(remember_token)
+      return false if remember_digest.nil?
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+  
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
+    
+    # フィード一覧を取得
+    def feed
+      Dish.where("user_id = ?", id)
+    end  
+  
   
   private
    
@@ -45,4 +53,4 @@ class User < ApplicationRecord
       self.email = email.downcase
     end
   
-end
+  end
